dec16c2b24174da688d0f0de162def01c3640772
HristoVasilev <ahristo.vasilev1999@gmail.com>
2020-08-07 18:34:14 +0300

    3 Sliders with JS and jQuery

---
+++ slider 1/js/script.js
@@ -0,0 +1,22 @@
+ 
+
+
+  const slides = document.querySelectorAll(".slide-item");
+  const slideTime = 3000;
+  let slideCounter = 0;
+  slides[slideCounter].classList.add("active");
+
+  setInterval(nextSlide,slideTime);
+
+  function nextSlide() {
+  	 slides[slideCounter].classList.remove("active");
+  	 if(slideCounter != slides.length-1){
+  	 	slideCounter++;
+  	 }
+  	 else{
+  	 	slideCounter = 0;
+  	 }
+  	 slides[slideCounter].classList.add("active");
+
+
+  }
